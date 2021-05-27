7eb739491989e34673d1cef59db57c9ee57f7fe0
prankshtain <mohammadasadiamin@gmail.com>
2021-05-27 22:54:39 +0430

    1.Creat functions instead raw codes 2.Check internet connection added to the app options 3.Check IP adress added to the app options (JUST FOR IR IP) 4.checking Youtube link validation  added to the app 5.Make variables more easier to read and understand 6.Improve user interface with using some colors 7.Some bugs fixes 8.A new file added for containing the functions in it (functions.py) NOTE: sorry for writing all these features in one commit  I forgot about it

--- YouTubeD.py
+++ YouTubeD.py
@@ -2,174 +2,90 @@ import os
 from pytube import YouTube
 import geocoder
 import requests
-def check_internet():
-    url='https://www.youtube.com/'
-    timeout=5
-    try:
-        _ = requests.get(url, timeout=timeout)
-        return True
-    except requests.ConnectionError:
-        os.system('color 4')
-    return False
-
-try:
-    while True:
-        check_connection=check_internet()
-        if check_connection == True :
-            g =geocoder.ip('me')
-            ip_info = str(g.geojson)
-            if 'IR' in ip_info:
-                os.system('color 4')
-                print('As you live in Iran please turn on your VPN or connect to a proxy and restart the program')
-                print("app closed!")
-                break
-            else:
-                os.system('color 2')
-                print("Ready to use")
-        else:
-            os.system('color 4')
-            print("please check your internet connection first and run the app again!")
-            print("app closed!")
-            break
-
-        # A Welcome text
-
-        #Get the link from user
-        l=1
-        while l == 1:
-            try:
-                link = input("enter the link => ")
-                os.system('color 7')
-                # make ready the link to Collecting data from youtube 
-                yl = YouTube(link)
-                l = 2
-            except:
-                print("please enter a valid url")
-        
-        print("collecting data")
-        # show the video title
-
-        print("Title :" , yl.title)
-
-        #show the video length
-
-        videoLength =int(yl.length)
-        print("Length :" , f"{int(videoLength / 60)+1} " + "min")
-        print("Data collected")
-
-        #Lets let the user choose the quality of his video 
-
-        h_or_l=input("If you want to download the video with the hoghest resolutoin you can write | highest | or | h |\n and for downloading the video with the lowest resolution you have to write | lowest | or | low  | \n you can download video with any resolution you want to with typing |dvr| or |diffrerent video resolutions| \n also you can download video and audio with diffrent resolution Separately, just write |dr| : ")
-        
-
-        if h_or_l == "low" or h_or_l == "lowest" or h_or_l == "lower":
-
-            # get the name of video that the user want to have with it in his dirctory
-
-            fname = input("Enter your video name : ")
-            fpath = input("enter your file path :")
-
-            # choose the lowest quality for the user with the pytube library
-
-            ys = yl.streams.get_lowest_resolution()
-            print("Start downloading")
-            ys.download(output_path=fpath,filename=fname)
-            print("Download finished")
-
-        elif h_or_l == "high" or h_or_l == "highest" or h_or_l == "higher":
-
-            # choose the highest quality for the user with the pytube library
-
-            fname = input("Enter your video name : ")
-            fpath = input("enter your file path :")
-            ys = yl.streams.get_highest_resolution()
-            print("start downloading")
-            ys.download(output_path=fpath,filename=fname)
-            print("Download finished")
-
-        elif h_or_l == "dr":
-
-            # choose the favorite quality for the user with the pytube library
+import functions as func
 
 
-            fname = input("Enter your video name : ")
-            faname = input("Enter your audio name : ")
-            reso=input("enter your ressolution number|720p,480p,360p,240p,144p| :")
-            fpath = input("enter your file path :")
-            if fpath == '':
-                fpath = None
+# try:
+while True:
+    ULR_VALIDATION = 0
+    check_connection=func.check_internet()
+    func.check_ip(check_connection)
+    youtube_video = func.ULR_VALIDATION(ULR_VALIDATION)
 
-            if reso == "720px" or reso == "720" or reso == "720p":
-                reso = "720p"
-            elif reso == "480px" or reso == "480" or reso == "480p":
-                reso = "480p"
-            elif reso == "360px" or reso == "360" or reso == "360p":
-                reso = "360p"
-            elif reso == "240px" or reso == "240" or reso == "240p":
-                reso = "240p"
-            elif reso == "144px" or reso == "144" or reso == "144p":
-                reso = "144p"
-            else:
-                reso = "720p"
-
-            print("Collecting data")
-            print(yl.streams.filter(res=reso))
-            print("Data collected")
-            itag = input("Enter your itag number : ")
-
-            # showing itags to user for choosing favorie quality
-
-            ysv = yl.streams.get_by_itag(itag)
-            print("Collecting your audio data")
-            print(yl.streams.filter(only_audio=True))
-            itag = input("Enter your itag number : ")
-            ysa = yl.streams.get_by_itag(itag)
-            print("start downloading video")
-            ysv.download(output_path=fpath,filename=fname)
-            print("video downloaded")
-            print("start downloading audio")
-            ysa.download(output_path=fpath,filename=faname)
-            print("audio downloaded")
-
-        elif h_or_l == "dvr" or h_or_l == "diffrerent video resolutions":
-            reso=input("enter your resolution : ")
-            if reso == "720px" or reso == "720" or reso == "720p":
-                reso = "720p"
-                print("Start downloading")
-                yl.streams.filter(res=reso).first().download()
-                print("Download finished")
-            elif reso == "480px" or reso == "480" or reso == "480p":
-                reso = "480p"
-                print("Start downloading")
-                yl.streams.filter(res=reso).first().download()
-                print("Download finished")
-            elif reso == "360px" or reso == "360" or reso == "360p":
-                reso = "360p"
-                print("Start downloading")
-                yl.streams.filter(res=reso).first().download()
-                print("Download finished")
-            elif reso == "240px" or reso == "240" or reso == "240p":
-                reso = "240p"
-                print("Start downloading")
-                yl.streams.filter(res=reso).first().download()
-                print("Download finished")
-            elif reso == "144px" or reso == "144" or reso == "144p":
-                reso = "144p"
-                print("Start downloading")
-                yl.streams.filter(res=reso).first().download()
-                print("Download finished")
-            else:
-                reso = "720p"
-                print("Start downloading")
-                yl.streams.filter(res=reso).first().download()
-                print("Download finished")
-        else:
-            fpath = input("enter your file path")
-            ys = yl.streams.get_highest_resolution()
-            print("Start downloading")
-            ys.download(output_path=fpath,filename=fname)
-            print("Download finished")          
-except:
-    print("App closed ")
+    #Get the link from user
+    print("collecting data")
     
-    print("Made by:pRANk3shTAIn  created in  :2021/3/23")
+    # show the video title
+    print("Title :" ,youtube_video.title )
+    
+    #show the video length
+    videoLength =youtube_video.length
+    print("Length :" , f"{videoLength} " + "second")
+    print("Data collected")
+    
+    #Lets let the user choose the quality of his video 
+    User_menu_choice=input("If you want to download the video with the hoghest resolutoin you can write | highest | or | h |\n and for downloading the video with the lowest resolution you have to write | lowest | or | low  | \n you can download video with any resolution you want to with typing |dvr| or |different rent video resolutions| \n also you can download video and audio with different nt resolution Separately, just write |dr| : ")
+    
+    if User_menu_choice == "low" or User_menu_choice == "lowest" or User_menu_choice == "lower":
+
+        # get the name of video that the user want to have with it in his dirctory
+        fname = input("Enter your video name : ")
+        fpath = input("enter your file path :")
+
+        # choose the lowest quality for the user with the pytube library
+        ys = youtube_video.streams.get_lowest_resolution()
+        print("Start downloading")
+        ys.download(output_path=fpath,filename=fname)
+        print("Download finished")
+
+    elif User_menu_choice == "high" or User_menu_choice == "highest" or User_menu_choice == "higher":
+
+        # choose the highest quality for the user with the pytube library
+        fname = input("Enter your video name : ")
+        fpath = input("enter your file path :")
+        ys = youtube_video.streams.get_highest_resolution()
+        print("start downloading")
+        ys.download(output_path=fpath,filename=fname)
+        print("Download finished")
+
+    elif User_menu_choice == "dr":
+
+        # choose the favorite quality for the user with the pytube library
+        fname = input("Enter your video name : ")
+        faname = input("Enter your audio name : ")
+        reso=input("enter your resolution number|720p,480p,360p,240p,144p| :")
+        fpath = input("enter your file path :")
+        if fpath == '':
+            fpath = None
+
+        func.check_reso(reso)
+
+        # showing video itags to user for choosing favorie quality
+        print("Collecting data")
+        print(youtube_video.streams.filter(only_video=True))
+        print("Data collected")  
+        itag = input("Enter your itag number : ")
+        video_choosen = youtube_video.streams.get_by_itag(itag)
+
+        # showing audio itags to user for choosing favorie quality
+        print("Collecting your audio data")
+        print(youtube_video.streams.filter(only_audio=True))
+        itag = input("Enter your itag number : ")
+        audio_choosen = youtube_video.streams.get_by_itag(itag)
+       
+        func.download_separately(video_choosen , audio_choosen , fname, fpath , faname)
+
+    elif User_menu_choice == "dvr" or User_menu_choice == "different  video resolutions":
+        reso=input("enter your resolution : ")
+        reso_final=func.check_reso(reso)
+        func.download_video(reso_final , youtube_video)
+    else:
+        fpath = input("enter your file path")
+        ys = youtube_video.streams.get_highest_resolution()
+        print("Start downloading")
+        ys.download(output_path=fpath,filename=fname)
+        print("Download finished")          
+# except:
+#     print("App closed ")
+    
+#     print("Made by:pRANk3shTAIn  created in  :2021/3/23")
---
+++ functions.py
@@ -0,0 +1,74 @@
+import os
+from pytube import YouTube
+import geocoder
+import requests
+import functions as func
+
+
+def check_internet():
+    url='http://www.google.com/'
+    timeout=5
+    try:
+        _ = requests.get(url, timeout=timeout)
+        return True
+    except requests.ConnectionError:
+        os.system('color 4')
+    return False
+    
+def check_ip(check_connection):
+    if check_connection == True :
+        g =geocoder.ip('me')
+        ip_info = str(g.geojson)
+        if 'IR' in ip_info:
+            os.system('color 4')
+            print('As you live in Iran please turn on your VPN or connect to a proxy and restart the program')
+            print("app closed!")
+            exit()
+        else:
+            os.system('color 2')
+            print("Ready to use")
+    else:
+        os.system('color 4')
+        print("please check your internet connection first and run the app again!")
+        print("app closed!")
+        exit()
+
+def ULR_VALIDATION(ULR_VALIDATION):
+    while ULR_VALIDATION == 0:
+        try:
+            link = input("enter the link => ")
+            os.system('color 7')
+            # make ready the link to Collecting data from youtube 
+            yl = YouTube(link)
+            ULR_VALIDATION = 1
+            return yl
+        except:
+            print("please enter a valid url")
+def check_reso(reso):
+    if "1080" in reso:
+        reso = "1080p"
+    elif "720" in reso :
+        reso = "720p"
+    elif "480" in reso :
+        reso = "480p"
+    elif "360" in reso :
+        reso = "360p"
+    elif "144" in reso :
+        reso = "144p"
+    else:
+        reso = "720p"
+    
+    return reso
+
+def download_video(final_reso , youtube_video):
+    print("Start downloading")
+    youtube_video.streams.filter(res=final_reso).first().download()
+    print("Download finished")
+
+def download_separately(video_choosen , audio_choosen , video_file_name , path , audio_file_name ):
+    print("start downloading video")
+    video_choosen.download(output_path=path,filename=video_file_name)
+    print("video downloaded")
+    print("start downloading audio")
+    audio_choosen.download(output_path=path,filename=audio_file_name)
+    print("audio downloaded")
